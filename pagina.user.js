897ac226e3d681a5e96dee98ae1de16a0ed743a1
nightwolfssa <167042382+nightwolfssa@users.noreply.github.com>
2024-04-14 21:59:33 -0300

    Create pagina.user.js

---
+++ pagina.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         RECARREGAR
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-14
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.segurobet.com/cassino/slots/395/395/playtech-live/26559-40013837-roleta-brasileira?mode=real&provider=all
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=segurobet.com
+// @grant        GM_log
+// ==/UserScript==
+
+function recarrega_pagina(){
+   var scriptUrl = GM_info.script.url;
+
+// Exibir a URL do script no console
+console.log('URL do script Tampermonkey:', scriptUrl);
+}
+
+setInterval(recarrega_pagina,10000)
